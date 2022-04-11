f4f4ede90d6086cdf226c1bebb7d943922cb0b12
Philip Kelley <philip@thoriumcube.org>
2022-04-11 21:08:34 +0100

    Add tests and update build file

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,11 +13,17 @@ repositories {
 dependencies {
     compileOnly("org.spongepowered:configurate-core:4.1.2")
     compileOnly("com.moandjiezana.toml:toml4j:0.7.2")
+    testCompileOnly("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testCompileOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+    testCompileOnly("org.spongepowered:configurate-core:4.1.2")
+}
+
+tasks.withType<Test> {
+    useJUnit()
 }
 
 tasks.withType<JavaCompile> {
     options.encoding = Charsets.UTF_8.name()
-    options.release.set(8)
 }
 
 tasks.withType<Javadoc> {
